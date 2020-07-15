1449d1398ddbb87c02e9244995d5385950402af1
Bonbia <bonbia75@gmail.com>
2020-07-15 18:08:13 +0200

    Image reddit fonctionnelle pub removed

---
+++ RED.js
@@ -0,0 +1,34 @@
+const tag = (type, opt = {}) => {
+    const t = document.createElement(type);
+
+    if (opt.className)
+        t.classList.add(opt.className);
+
+    if (opt.href)
+        t.setAttribute('href', opt.href);
+
+    if (opt.innerText)
+        t.innerText = opt.innerText;
+
+    return t;
+}
+async function main() {
+    const response = await fetch(`https://www.reddit.com/r/memes/.json`);
+    const obj = await response.json();
+
+    console.log(obj);
+    const post = obj.data.children[Math.floor(Math.random() * obj.data.children.length)];
+    const postDiv = document.createElement('div');
+    postDiv.classList.add('post');
+
+    const postImage = document.createElement('img');
+    postImage.id="meme";
+    postImage.src=post.data.url;
+    postDiv.appendChild(postImage);
+    document.getElementById("meme2").appendChild(postDiv);  //ca fait apparaitre la tout a la fin du body
+    
+    
+
+}
+
+main();
